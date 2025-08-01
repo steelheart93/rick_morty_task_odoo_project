e8110758da6b0b209ed57d38a0a411127affe6f8
Stiven Muñoz Murillo <stiven070893@gmail.com>
2025-07-31 23:09:32 -0500

    modulo de odoo

---
+++ odoo_rick_tasks/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Rick Tasks',
+    'version': '1.0',
+    'category': 'Productivity',
+    'summary': 'Tareas con personajes de Rick and Morty',
+    'author': 'Stiven Muñoz',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
